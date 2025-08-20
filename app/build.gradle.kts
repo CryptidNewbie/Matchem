7070bd971b5e73f0bb85658892e5ff9239bfc6fe
Ithok1 <Mikes@CryptidMD.com>
2025-08-20 13:22:35 -0400

    Gradle upgrades

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.cryptidnewbie.matchem"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.cryptidnewbie.matchem"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
